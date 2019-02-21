0bce2004d3002688a34fd02c960cadc570c64ca7
CristinaVeronica <cristina.pavelescu93@gmail.com>
2019-02-21 21:40:39 +0200

    Curs 1

---
+++ Exemplu7.java
@@ -0,0 +1,8 @@
+public class Exemplu7 {
+    
+    public static void main(String [] args) { // aici incepe declararea clasei
+        String x = 3 > 6 ? "Hello" : "World";
+        
+        System.out.println(x);
+    }
+}
